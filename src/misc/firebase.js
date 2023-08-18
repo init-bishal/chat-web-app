0affdbd5137a64cf61a42673accdf3ace775edf9
Bishal Ghosh <bbishal.g01@gmail.com>
2023-08-18 14:29:11 +0530

    firebase.js

---
+++ src/misc/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase/app'
+const config = {
+    apiKey: "AIzaSyCru3nqpWaT_w1-Qka-NVU5biPCYEM16hA",
+    authDomain: "chat-web-app-2a3e4.firebaseapp.com",
+    databaseURL: "https://chat-web-app-2a3e4-default-rtdb.firebaseio.com",
+    projectId: "chat-web-app-2a3e4",
+    storageBucket: "chat-web-app-2a3e4.appspot.com",
+    messagingSenderId: "556737306542",
+    appId: "1:556737306542:web:339f0d014457d39954323f"
+};
+const app=firebase.initializeApp(config)
